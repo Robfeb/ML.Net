728d3d60aba69b0d0bfde308517ae10f78fbab37
Roberto Febrer <febrer@gmail.com>
2020-10-04 21:48:31 +0200

    add ML project. builder & tester

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ML.Net
-Tutorial for ML.Net
+Sentiment Analysis Tutorial for ML.Net
+[Link](https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/intro)
+
+
---
+++ myMLApp/consumeModelApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SampleClassification.Model;
+
+namespace consumeModelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            Console.WriteLine("Please, Write a phrase to analize it and provide a sentiment");
+            input.SentimentText = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            for (int i = 0; i < result.Score.Length; i++)
+            {
+                Console.WriteLine($"\nScore: {result.Score[i]}\nI: {i}");
+            }
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
+
